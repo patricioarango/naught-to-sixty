2a0dfdbc6a1960f42b4b3a86d89d48b618e5858c
patricioarango <patricioarango@gmail.com>
2016-09-13 20:59:35 -0300

    two

--- www/js/bytimecontroller.js
+++ www/js/bytimecontroller.js
@@ -1,5 +1,5 @@
 
-aplicacion.controller('ByTimeController',['$scope','$location','$http','$window' function($scope,$location,$http,$window){
+aplicacion.controller('ByTimeController',['$scope','$location','$http','$window', function($scope,$location,$http,$window){
 	var geooptions = { timeout: 30000,enableHighAccuracy: true };
 
         watchID = $window.navigator.geolocation.watchPosition(function(position){
