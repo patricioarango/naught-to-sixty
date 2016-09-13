2f1d5ffb0588c4f831e78bfcfb887ea30ec89aca
patricioarango <patricioarango@gmail.com>
2016-09-13 17:39:10 -0300

    two

--- www/js/bydistancecontroller.js
+++ www/js/bydistancecontroller.js
@@ -1,4 +1,11 @@
 
 aplicacion.controller('ByDistanceController',['$scope','$location','$http', function($scope,$location,$http){
+	$scope.mostrar_otro = false;
 
+	$scope.quiere_otro = function(){
+		$scope.mostrar_otro = true;		
+	}
+
+	console.log("$scope.cantidad_metros");
+	console.log($scope.cantidad_metros);
 }]);
