f48977b315926445e1a4fbf2ac76014f9146616f
patricioarango <patricioarango@gmail.com>
2016-09-13 16:02:22 -0300

    two

--- www/js/bytimecontroller.js
+++ www/js/bytimecontroller.js
@@ -1,4 +1,23 @@
 
 aplicacion.controller('ByTimeController',['$scope','$location','$http', function($scope,$location,$http){
+    var geooptions = { timeout: 30000,enableHighAccuracy: true };
+    watchID = navigator.geolocation.watchPosition(onSuccess, onError,geooptions);
 
+        $scope.onSuccess = function(position) {
+        	var tiempo = formatear_timestamp(position.timestamp);
+        	var d = new Date(position.timestamp);
+        	var n = d.toTimeString();
+	        var current_speed = (position.coords.speed * 3.6).toFixed(2); 
+    	    $("#speed_contenedor").text(current_speed + " km/h");
+        	$(".determinate").css("width", current_speed);
+        	control_velocidad(current_speed);
+    	};
+    	$scope.control_velocidad(velocidad) = function(){
+    		$scope.velocidad = velocidad;
+    	}
+    	// onError Callback receives a PositionError object
+    	$scope.onError = function(error) {
+	        alert('code: '    + error.code    + '\n' +
+              'message: ' + error.message + '\n');
+    	}    	
 }]);
--- www/js/index.js
+++ www/js/index.js
@@ -34,11 +34,7 @@ var app = {
         
     },onDeviceReady: function() {
         console.log("deviceready");
-
-        var geooptions = { timeout: 30000,enableHighAccuracy: true };
-
-        watchID = navigator.geolocation.watchPosition(onSuccess, onError,geooptions);
-
+        
         var pushNotification = window.plugins.pushNotification;
         pushNotification.register(app.successHandler, app.errorHandler,{"senderID":"391779146922","ecb":"app.onNotificationGCM"});
         },successHandler: function(result) {
@@ -72,31 +68,9 @@ var app = {
         }
 };//devideready
 
-    var onSuccess = function(position) {
-        var tiempo = formatear_timestamp(position.timestamp);
-        var d = new Date(position.timestamp);
-        var n = d.toTimeString();
-        /*$("#geo_values").html('<p>Latitude: '          + position.coords.latitude          + ' ' +
-              'Longitude: '         + position.coords.longitude         + ' ' +
-              'Altitude: '          + position.coords.altitude          + ' ' +
-              'Accuracy: '          + position.coords.accuracy          + ' ' +
-              'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + ' ' +
-              'Heading: '           + position.coords.heading           + ' ' +
-              '<strong>Speed: </strong>'             + (position.coords.speed * 3.6)             + ' ' +
-              'Timestamp: '         + tiempo                + ' '+
-              'Hora normal: ' + n);*/
-        var current_speed = (position.coords.speed * 3.6).toFixed(2); 
-          $("#speed_contenedor").text(current_speed + " km/h");
-          $(".determinate").css("width", current_speed);
-        control_velocidad(current_speed);
-    };
 
-    // onError Callback receives a PositionError object
-    //
-    function onError(error) {
-        alert('code: '    + error.code    + '\n' +
-              'message: ' + error.message + '\n');
-    }
+
+
 
 function formatear_timestamp(timestamp){
   //console.log("timestamp");
