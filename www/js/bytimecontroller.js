c7733dd62a679b39a1314d484ac41c059421243a
patricioarango <patricioarango@gmail.com>
2016-09-13 17:23:29 -0300

    two

--- www/js/bytimecontroller.js
+++ www/js/bytimecontroller.js
@@ -2,5 +2,5 @@
 aplicacion.controller('ByTimeController',['$scope','$location','$http', function($scope,$location,$http){
 	console.log("en controller");
 	console.log(aplicacion.velocidad);
-	$scope.velocidad = aplicacion.velocidad;
+	$scope.velocidad = "la puta madre";
 }]);
